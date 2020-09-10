7c31055fecede8dd506d316fd6481c11989c8dd4
emonzon42 <eli.develops@gmail.com>
2020-09-10 13:18:35 -0400

    did #5 dictionaries

--- Homework/HW0/hw0.py
+++ Homework/HW0/hw0.py
@@ -26,7 +26,11 @@ def three_tuples(S):
     return [(i,j,k) for i in S for j in S for k in S if (i+j+k == 0)]
 
 #5 Dictionaries
+def dict_init():
+    return {'Neo':'Keanu', 'Morpheus':'Laurence', 'Trinity':'Carrie-Anne'}
 
+def dict_find(dlist, k):
+    return [i[k] if k in i else 'not present' for i in dlist]
 
 #6 Filereading
 def file_line_count(filename):
@@ -38,12 +42,16 @@ def file_line_count(filename):
     f.close()
     return i
 
-
+#7 Mini Search Engine
 
 if __name__ == '__main__':
     test_print()
     list_set_length()
     print(set_intersect( {1,2,3,3}, {3,4,5,6}))
     print(three_tuples({-4,-2, 1, 2, 5, 0}))
+    mydict = dict_init()
+    mydict['Neo'] = 'DegrassiTyson'
+    print(mydict['Neo'])
+    print(dict_find([mydict,{'first':'1', 'Neo':'2'}], 'Neo'))
     print(file_line_count('Homework/HW0/stories.txt'))
     
